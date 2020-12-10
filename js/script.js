dd4b0a5a19012c096bb74cd0e5277052427d5b1f
maslana.m <maslana.m89@gmail.com>
2020-12-10 15:25:21 +0100

    Matching pairs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
 const cards = document.querySelectorAll('.card');
-console.log(cards);
 
-cards.forEach((card) => card.addEventListener('click', showCard));
+let activeCard = '';
+const activeCards = [];
 
-function showCard() {
-  const exact = this.querySelector('.front');
-  console.log(exact);
+function matchCards() {
+  console.log('wywolana');
+  let isMatched = activeCards[0].dataset.name === activeCards[1].dataset.name;
+  if (!isMatched) {
+    activeCards.forEach((card) => card.classList.remove('flip'));
+    console.log('nie ma pary!');
+  } else if (isMatched) {
+    console.log('trafiony-zatopiony');
+  }
+
+  activeCard = '';
+  activeCards.length = 0;
+  cards.forEach((card) => card.addEventListener('click', showCards));
 }
+
+function showCards() {
+  activeCard = this;
+  activeCard.classList.add('flip');
+
+  if (activeCard == activeCards[0]) {
+    return;
+  }
+
+  if (activeCards.length === 0) {
+    console.log('first card');
+    activeCards[0] = activeCard;
+    return;
+  } else {
+    console.log('second card');
+    cards.forEach((card) => card.removeEventListener('click', showCards));
+    activeCards[1] = activeCard;
+  }
+
+  setTimeout(matchCards, 1500);
+}
+
+cards.forEach((card) => card.addEventListener('click', showCards));
